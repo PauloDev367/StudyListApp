bb580064e92e92dfd989fc7565f961a38ac708ac
Paulo Silva <pauloecomercial367@gmail.com>
2025-10-01 13:20:21 -0300

    style: criando layout padrão do projeto e configurando assets

--- src/data/db_context.py
+++ src/data/db_context.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__, template_folder='../views')
+app = Flask(__name__, template_folder='../views', static_folder='../../assets')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
